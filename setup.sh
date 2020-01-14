ddea5d340f70fa8f834827e39ac3c69b95a504c1
baby_gang <b.gang.k@gmail.com>
2020-01-14 17:29:27 +0900

    gitの設定を修正 - gitattributesとgitignore_globalを追加 - gitdiffで画像の差分を出すためのスクリプトを追加

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 function mksymln() {
-  unlink ~/$1 2>/dev/null
-  ln -s `pwd`/$1 ~/
+  local dirpath=${2:-~}
+  unlink $dirpath/$1 2>/dev/null
+  ln -s `pwd`/$1 $dirpath/
 }
 
 mksymln .vimrc
 mksymln .bashrc
 mksymln .bash_profile
 mksymln .gitconfig
+mksymln .gitattributes
+mksymln .gitignore_global
+mksymln git-imgdiff /usr/local/bin
